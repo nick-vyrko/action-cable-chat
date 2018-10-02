559eb0fa20a446373cbbe68c12f8a3b57acfd047
Nick Vyrko <nikita.vyrko@gmail.com>
2018-10-02 15:42:43 +0300

    Add limit at a meesage

--- app/assets/javascripts/sendMessage.js
+++ app/assets/javascripts/sendMessage.js
@@ -1,14 +1,23 @@
+MAX_LENGTH_MESSAGE = 120;
+
 function sendMessage(e, chatroomId) {
-  if(e.key === "Enter" && !e.shiftKey) {
+  const textarea = e.currentTarget;
+  const value = trimStr(textarea.value);
+  const alert = document.querySelector(".alert");
+
+  if (value.length >= MAX_LENGTH_MESSAGE) {
+    const new_alert =  alert || createHTML(`<div class='alert alert-danger'>Max message length is ${MAX_LENGTH_MESSAGE}</div>`)
+    textarea.parentNode.appendChild(new_alert);
+    return;
+  } else {
+    alert && textarea.parentNode.removeChild(alert);
+  }
+
+  if (value.length > 0 && e.key === "Enter" && !e.shiftKey) {
     e.preventDefault();
-
-    let textarea = e.currentTarget;
-    const value = trimStr(textarea.value);
-    if (value) {
       App.chatrooms[chatroomId].send({ user_id: textarea.dataset.userId, message: value });
-    }
 
-    textarea.value = "";
+      textarea.value = "";
   }
 }
 
