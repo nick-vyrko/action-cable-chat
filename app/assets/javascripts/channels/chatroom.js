d9e7b9f06db75c700608af45731b264623a3b6b3
Nick Vyrko <nikita.vyrko@gmail.com>
2018-10-14 15:14:52 +0300

    [#1] Do not create new connection on chat opening

--- app/assets/javascripts/channels/chatroom.js
+++ app/assets/javascripts/channels/chatroom.js
@@ -1,5 +1,10 @@
 document.addEventListener("turbolinks:load", function() {
-  const id = document.querySelector('#message_form').dataset.chatroomId;
+  const messageForm = document.querySelector('#message_form');
+  if (!messageForm) { return; }
+
+  const id = messageForm.dataset.chatroomId;
+  if (App.chatrooms[id]) { return; }
+
   App.chatrooms[id] = App.cable.subscriptions.create({channel: "ChatroomChannel", room_id: id}, {
     received: function(data) {
       const list = document.querySelector("#messages_list");
@@ -12,10 +17,10 @@ document.addEventListener("turbolinks:load", function() {
 const createMessage = (data) => {
   const messageString = `<div class="message-row"><div class="message-item"><div class="name">${data.name}</div><div class="content">${data.content}</div></div></div>`;
   return createHTML(messageString);
-}
+};
 
 const createHTML = (domstring) => {
   const html = new DOMParser().parseFromString(domstring, 'text/html');
   return html.body.firstChild;
-}
+};
 
